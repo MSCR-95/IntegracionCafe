370e44a8b799c974f10fd6e3ed17f8ddd698f84e
Maria S <Maria S@DESKTOP-QB1MIM4>
2023-11-03 16:27:09 +0100

    cambios

--- src/Tasks/Replicator.java
+++ src/Tasks/Replicator.java
@@ -1,14 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Tasks;
 
 import Slot.Slot;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import org.w3c.dom.Document;
 
 /**
  *
--- src/Tasks/Task.java
+++ src/Tasks/Task.java
@@ -1,12 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Tasks;
 
 import Slot.Slot;
-import java.util.ArrayList;
-import java.util.List;
 import org.w3c.dom.Document;
 import org.w3c.dom.ls.DOMImplementationLS;
 import org.w3c.dom.ls.LSSerializer;
